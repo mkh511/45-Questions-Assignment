ce36eac7ea903ff93396b9d4cd9062911fa934bd
Muhammad Khalid Hussain <khalidks29@gmail.com>
2024-03-22 11:04:32 +0500

    My first Commit

---
+++ assignment2.js
@@ -0,0 +1,9 @@
+"use strict";
+// store a person name in a variable
+let personname = "Muhammad Khalid Hussain";
+// Print Person name in UpperCase
+console.log("LowerCase:", personname.toLowerCase());
+// Print Person name in UpperCase
+console.log("UpperCase:", personname.toUpperCase());
+// Print Person name in TitleCase
+console.log("titlecase:", personname.totitlecase());
---
+++ assignment3.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing the famous code and its author
+let qoute = "Its never too late.";
+let author = "Muhammad Khalid Hussain";
+//printing the message
+console.log(`"${qoute}" - ${author}`);
